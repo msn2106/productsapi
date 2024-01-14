4f4332b11186f83688555e5591f032e9f6718c00
Mayank Singh <msingh@cogitate.us>
2024-01-14 21:07:05 +0530

    ref#101 - initial code till get and put ep

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # productsapi
 REST API made using Express and JSON Placeholder to provide dummy products data.
+
+GET /api/products - List all products - DONE
+GET /api/products/1 - Get details of product with id = 1 - DONE
+
+POST /api/products - Create a new product - 
+
+PATCH /api/products/1 - Edit the product with id = 1
+
+DELETE /api/products/1 - Delete the product with id = 1
---
+++ index.js
@@ -0,0 +1,76 @@
+const express = require("express");
+const products = require("./products.json");
+const fs = require("fs");
+const { randomUUID } = require("crypto");
+const app = express();
+
+app.use(express.json());
+
+const PORT = 8080;
+
+// Middleware - Plugin
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/products", (req, res) => {
+  const html = `
+  <ul>
+    ${products.map((product, index) => `<li>${product.productName}</li>`).join("")}
+  </ul>
+  `;
+  res.status(200).send(html);
+});
+
+// REST APIs
+app.get("/api/products", (req, res) => {
+  return res.json(products);
+});
+
+app.get("/api/products/:id", (req, res) => {
+  const { id } = req.params;
+  const product = products.find((x) => x.id === Number(id));
+  if (!product)
+    return res.json({
+      error: `No Product found with id ${id}`,
+    });
+  return res.json(product);
+});
+
+app.post("/api/products", (req, res) => {
+  const body = req.body;
+  products.push({ ...body, id: products.length + 1, productPrice: parseFloat(body.productPrice) });
+  fs.writeFile("./products.json", JSON.stringify(products), (err, data) => {
+    console.log(`${err} occurred while writing data to file`);
+    return res.json({
+      status: 200,
+      message: `Product added successfully with id ${products.length}`,
+    });
+  });
+});
+
+app.patch("/api/products/:id", (req, res) => {
+  let { id } = req.params;
+  // const products = products.filter(x => x.id !== id);
+  // fs.writeFile('./')
+  return res.json({
+    status: 200,
+    message: `Product with id ${id} updated successfully`,
+  });
+});
+
+app.delete("/api/products/:id", (req, res) => {
+  let { id } = req.params;
+  console.log(`deleted ${id}`);
+  const tempProducts = products.filter((x) => x.id !== id);
+  console.log(tempProducts);
+  fs.writeFile("./products.json", JSON.stringify(tempProducts), (err, data) => {
+    console.log(`${err} occurred while writing data to file`);
+    return res.json({
+      status: 200,
+      message: `Product with id ${id} Deleted successfully`,
+    });
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`API is running on PORT ${PORT}`);
+});
